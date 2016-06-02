b5433b526879714fa966e1dd5000e1fd51759cd5
Gildênio Neto <netok99@gmail.com>
2016-06-01 21:24:07 -0300

    first commit

---
+++ ChoosePet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChoosePet
+//
+//  Created by Gildênio Reis do Nascimento Neto on 01/06/16.
+//  Copyright © 2016 choosepet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'MyApp' do
+  pod 'SwiftyJSON', '~> 2.3'
+  pod 'Firebase'
+end
